2e15a34ccecd83f371208cacc92fe2436501d994
Jose Garcia <jose.garcia@agileinnova.org>
2022-06-20 21:03:48 -0500

    first commit

---
+++ models/busquedas.js
@@ -0,0 +1,109 @@
+const fs = require('fs');
+const axios = require('axios');
+
+class Busquedas {
+    historial = [];
+    dbPath = "./db/database.json";
+
+    constructor() {
+        // TODO: leer DB si existe
+        this.leerDB();
+    }
+
+    get historialCapitalizado() {
+        // Capitalizar cada palabra
+        return this.historial.map( lugar => {
+            let palabras = lugar.split(' ');
+            palabras = palabras.map( palabra => palabra[0].toUpperCase() + palabra.substring(1) )
+            return palabras.join(' ');
+        });
+    }
+
+    get paramsMapbox () {
+        return {
+            "access_token": process.env.MAPBOX_KEY,
+            "limit": 5,
+            "language": 'es'
+        }
+    }
+
+    get paramsWeather () {
+        return {
+            "appid": process.env.OPENWEATHER_KEY,
+            "units": "metric",
+            "lang": "es"
+        }
+    }
+
+    async ciudad ( lugar = '' ) {
+        // petición http
+        try {
+            const intance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
+                params: this.paramsMapbox
+            })
+            const resp = await intance.get();
+            return resp.data.features.map( lugar => ({
+                id: lugar.id,
+                nombre: lugar.place_name,
+                lng: lugar.center[0],
+                lat: lugar.center[1]
+            }));
+        } catch (err) {
+            return []; // retornar los lugares
+        } 
+    }
+
+    async climaLugar( lat, lon ) {
+        try {
+            // intance axios.create()
+            const instance = axios.create({
+                baseURL: "https://api.openweathermap.org/data/2.5/weather",
+                params: { ...this.paramsWeather, lat, lon }
+            })
+            // resp.data
+            const resp = await instance.get();
+            const { weather, main } = resp.data;
+            return {
+                desc: weather[0].description,
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp
+            }
+            
+        } catch (err) {
+            console.log( err );
+        }
+    }
+
+    agregarHistorial( lugar = '' ) {
+        // TODO: prevenir duplicados
+        if ( this.historial.includes( lugar.toLocaleLowerCase() ) ) return;
+
+        this.historial = this.historial.splice(0,5);
+        this.historial.unshift( lugar.toLocaleLowerCase() );
+
+        // Grabar en DB
+        this.guardarDB()
+    }
+
+    guardarDB() {
+        const payload = {
+            historial: this.historial
+        }
+        fs.writeFileSync( this.dbPath, JSON.stringify( payload ) );
+    }
+
+    leerDB() {
+        // Debe de existir...
+        if ( !fs.existsSync( this.dbPath ) ) return;
+
+        const info = fs.readFileSync( this.dbPath, { encoding: 'utf-8' } );
+        const data = JSON.parse( info );
+        this.historial = data.historial;
+
+    }
+
+}
+
+module.exports = Busquedas;
